7ba4f83c6c479ed2194bb25320883581e0c15a00
neardws <singleangel@vip.qq.com>
2017-10-30 11:29:27 +0800

    add readme

--- app/src/main/java/com/qq/vip/singleangel/communcationwithserver/MainActivity.java
+++ app/src/main/java/com/qq/vip/singleangel/communcationwithserver/MainActivity.java
@@ -1,5 +1,6 @@
 package com.qq.vip.singleangel.communcationwithserver;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +11,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * 连接服务器的Demo
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private Button btConnect;
-    private TextView tvMessage;
+    private Button btConnect;   //连接按钮
+    private TextView tvMessage;  //显示从接收服务器的数据
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,31 +32,62 @@ public class MainActivity extends AppCompatActivity {
         btConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Socket socket = null;
-                try {
-                    InetAddress serverAdd = InetAddress.getByName("120.78.167.211");
-                    socket = new Socket(serverAdd,34567);
-                    //接受服务器信息
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()));
-                    //得到服务器信息
-                    String msg = in.readLine();
-                    tvMessage.setText(msg);
-                    in.close();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }finally {
-                    try{
+                /**
+                 * 注意这里因为需要连接Socket和对UI进行操作。所以使用AsyncTask
+                 * 在main process执行会报错 在主线程进行网络操作
+                 * 在Thread中执行会报错，在非主线程执行UI操作，即tvMessage.setText
+                 */
+                connectAsyncTask asyncTask = new connectAsyncTask(tvMessage);  //
+                asyncTask.execute();
+            }
+        });
+    }
+
+
+
+    public class connectAsyncTask extends AsyncTask<Void,Void,String>{
+        private final TextView textView;
+
+        public connectAsyncTask(TextView textView){
+            this.textView = textView;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            Socket socket = new Socket();  //新建Socket
+            try {
+                socket.bind(null);
+                /**
+                 * 连接时需要IP address. port . 5000是重连时间，应该是5S
+                 */
+                socket.connect(new InetSocketAddress("120.78.167.211",34567),5000);
+                //接受服务器信息
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                //得到服务器信息
+                String msg = in.readLine();
+                in.close();
+                return msg;
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                if (socket != null) {
+                    try {
                         socket.close();
-                    }catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-
             }
-        });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            textView.setText(s);
+        }
     }
 
 }
